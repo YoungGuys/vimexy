4b2d963526a055cb0c4320769f998f070e4ae164
verblendung <art.shramko@gmail.com>
2015-02-07 23:43:51 +0200

    REGISTER html

--- view/modal.php
+++ view/modal.php
@@ -16,7 +16,26 @@
 						<textarea name="" id="" cols="30" rows="10"></textarea>
 					</p>
 					<p>
-
+						<label>Фільтри (зажміть shift для вибору декількох фільтрів)</label>
+						<select multiple>
+							<option>Допомога дітям</option>
+						</select>
+					</p>
+					<p>
+						<label>Дата початку</label>
+						<input type="date">
+					</p>
+					<p>
+						<label>Дата кінця</label>
+						<input type="date"/>
+					</p>
+					<p>
+						<label>Місце проведення</label>
+						<input type="text"/>
+					</p>
+					<p>
+						<label>Кількість людей</label>
+						<input type="num">
 					</p>
 				</div>
 				<div class="modal__footer">
