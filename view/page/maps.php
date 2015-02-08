508897246af25e585d3900ecc2fa2b8305b69d0f
Коля <kolka.koval@gmail.com>
2015-02-08 14:08:11 +0200

    page map

---
+++ view/page/maps.php
@@ -0,0 +1,21 @@
+<section class="page">
+
+
+    <form class="page__filter"></form>
+
+
+
+    <div class="page__my_events">
+
+        <script>
+            gMap(lat, lon);
+            gMap(lat, lon);
+            gMap(lat, lon);
+            gMap(lat, lon);
+        </script>
+
+        <div id="map" class="g-maps"></div>
+
+    </div>
+
+</section>
