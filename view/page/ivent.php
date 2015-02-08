5c6b305b6433264f97ee6004a27ef3157cb36a19
Balon4ik <gunter0104@gmail.com>
2015-02-08 09:37:26 +0200

    Event list ready

--- view/page/ivent.php
+++ view/page/ivent.php
@@ -3,7 +3,7 @@
 
     <div class="event">
         <div class="event__header">
-            <h2 class="event__header__title">Олена Іванова</h2>
+            <h2 class="event__header__title"><?=$data->get('title');?></h2>
             <span class="event__header__users">
                 <i class="i-user"></i>
                 0
@@ -11,14 +11,14 @@
         </div>
 
         <div class="event__col-1">
-            <img class="event__pic" src="" alt="event-img"/>
+            <img class="event__pic" src="<?=$data->get('photo');    ?>" alt="event-img"/>
 
             <a class="btn btn--green event__btn_add" href=""></a>
 
             <div class="event__descr">
                 <div class="event__descr__title">Опис</div>
                 <div class="event__descr__text">
-                    Терміново потрібна няня для 2-річної дитини на один день. Мати дитини лягає на однодневну операцію, знайомих та родичів в Києві немає, з ким би можливо було залишити дитину. Дитину потрібно тричи нагодувати, піти на прогулянку, а також покласти спати в обід та ввечері. Дитина дуже слухняна, ласкава, звати Софія. Сподіваємося на вашу допомогу!
+                    <?=$data->get('about');?>
                 </div>
             </div>
 
@@ -45,32 +45,33 @@
             </div>
 
             <div class="event__date">
-                <time class="event__date__title">5 березня 2015 р.</time>
+                <time class="event__date__title"><?=$data->date;?></time>
                 <i class="i-calendar event__date__calendar"></i>
                 <div class="event__date__info">
-                    <span class="c-blue">усього:</span> 11 годин
-                    <span class="c-blue">з:</span> 8:00
-                    <span class="c-blue">до:</span> 19:00
+                    <span class="c-blue">з:</span> <?=$data->time_s;?>
+                    <span class="c-blue">до:</span> <?=$data->time_f;?>
                 </div>
             </div>
 
             <div class="event__contact">
                 <div class="event__contact__title">Контакти</div>
-                <img class="event__contact__img" src="" alt="user-img"/>
+                <img class="event__contact__img" src="<?=$user->get("photo");?>" alt="user-img"/>
                 <div class="event__contact__info">
                     <p>
                         <i class="i-tel"></i>
-                        093-973-05-19
+                        <?php if ($user->get('phone')) echo $user->get('phone'); ?>
                     </p>
                     <p>
                         <i class="i-fb-blue"></i>
-                        valdfs_dsfds
+                        <?php if ($user->get('website')) echo $user->get('sebsite') ?>
                     </p>
                     <p>
-                        i.i-mail
+                        <?php if ($user->get('email')) echo $user->get('email');?>
                     </p>
                 </div>
-                <a class="event__contact__link-avtor" href="">Валенти</a>
+                <a class="event__contact__link-avtor" href="">
+                    <?php if ($user->get('firstName')) echo $user->get('firstName'); else echo $user->get('name');?>
+                </a>
             </div>
         </div>
 
@@ -78,14 +79,16 @@
         <div class="profile__comment">
             <h6 class="profile__comment__title">Відгуки про мене</h6>
             <ul class="profile__comment__list">
-                <li class="profile__comment__list__item">
-                    <img class="profile__comment__list__item__avatar" src="" alt="avatar-user"/>
-                    <div class="profile__comment__list__item__info">
-                        <p class="profile__comment__list__item__name">Бред Пит</p>
-                        <time class="profile__comment__list__item__date">07.02.15</time>
-                        <p class="profile__comment__list__item__text">jkdfgfbfdgfdbghbgdfhj</p>
-                    </div>
-                </li>
+                <?php foreach ($comments as $key => $comment) { ?>
+                    <li class="profile__comment__list__item">
+                        <img class="profile__comment__list__item__avatar" src="" alt="avatar-user"/>
+                        <div class="profile__comment__list__item__info">
+                            <p class="profile__comment__list__item__name"><?=$commentsUser->get($comment->get("authorID")->getObjectId())?></p>
+                            <time class="profile__comment__list__item__date">07.02.15</time>
+                            <p class="profile__comment__list__item__text">jkdfgfbfdgfdbghbgdfhj</p>
+                        </div>
+                    </li>
+                <?php } ?>
             </ul>
 
         </div>
