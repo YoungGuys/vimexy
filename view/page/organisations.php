5698ecb3c2a1f9af26c847f8fbcf7d4adf845c5c
MiKolka <kolka.koval@yandex.ru>
2015-07-26 23:11:50 +0300

    add page organisation

---
+++ view/page/organisations.php
@@ -0,0 +1,84 @@
+<section class="page">
+
+    <form class="page__filter" action="" method="get">
+        <div class="page__filter__item page__filter__item--big">
+            <a class="btn btn--two btn--big btn--yellow" href="">Організіція</a>
+            <a class="btn btn--two btn--big btn--border" href="">Компанія</a>
+        </div>
+
+        <div class="page__filter__item page__filter__item--big">
+            <select class="page__filter__item__select btn btn--two" name="">
+                <option value="">Сфера діяльності</option>
+            </select>
+
+            <select class="page__filter__item__select btn btn--two" name="">
+                <option value="">Сортувати за</option>
+            </select>
+        </div>
+
+        <div class="page__filter__item">
+            <button class="btn btn--big btn--border">Створити</button>
+        </div>
+
+        <div class="page__filter__item page__filter__item--search">
+            <input class="btn btn--blue btn-search" value="Пошук" type="submit"/>
+        </div>
+    </form>
+
+
+    <div class="page__events">
+
+        <div class="page__events__item">
+            <div class="page__events__item__pic">
+                <img class="page__events__item__pic__img" src="" alt="event-img"/>
+            </div>
+            <div class="page__events__item__info">
+                <h4 class="page__events__item__info__title">
+                    <a href="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </a>
+                </h4>
+
+                <p class="page__events__item__info__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci delectus dolorem dolores dolorum
+                    ex fuga fugit in ipsa, iure, maxime, mollitia nostrum qui ratione repudiandae tenetur velit veniam.
+                    Hic, id!
+                </p>
+
+                <p class="page__events__item__info__organizator">
+                    Сайт:
+                    <a class="page__events__item__info__organizator__link"
+                       href="#">
+                        www.site.com
+                    </a>
+                </p>
+            </div>
+            <div class="page__events__item__meta">
+                <p class="page__events__item__meta__user">
+                    <i class="i-user"> </i> 1509
+                </p>
+
+                <p class="page__events__item__meta__user c-grey">
+                    Сфера:
+                    <a href="#">молодіжна</a>
+                </p>
+
+                <p class="page__events__item__meta__user c-grey">
+                    Місто:
+                    <a href="#">Київ</a>,
+                    <a href="#">Харків</a>,
+                    <a href="#">Одеса</a>,
+                    <a href="#">Львів</a> ...
+                </p>
+
+                <a class="btn btn--blue btn--full page__events__item__meta__btn"
+                   href="">
+                    Детальніше
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+
+</section>
